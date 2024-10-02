7d55eebd79dce9f10a27a89e26dccf455b4da2e5
Manikandan <manikandan.arumugam@tisteps.co>
2024-10-02 23:29:02 +0530

    resume change

--- src/components/context/MyResumeContext.jsx
+++ src/components/context/MyResumeContext.jsx
@@ -24,7 +24,7 @@ const MyResumeContextProvider = ({ children }) => {
   ];
 
   const gitHubLinks = [
-    "https://manikeeranur.github.io/Portfolio/",
+    "https://manikandan-portfolio-ruby.vercel.app/",
     "https://manikeeranur.github.io/Reactproject1/",
   ];
 
